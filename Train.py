54b9deee5dd5d092c26c5230cbf0592dc2d3d236
kiranbanstola <banstolakiran11@gmail.com>
2022-02-08 23:04:11 +0545

    Training Dataset By Classifier and Saving

--- Train.py
+++ Train.py
@@ -1,11 +1,31 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 from tkinter import messagebox
 import cv2
 import os
 import numpy as np
 
-class Train:
+
+class Train_Dataset:
+    def __init__(self, screen):
+        self.screen = screen
+        self.screen.geometry("400x200+200+150")
+        self.screen.title("Train Dataset")
+        self.screen.resizable(False, False)
+
+        # bg_gradient
+        img1 = Image.open(r"D:\FaceDetectionAndRecognition-v2\bg_gradient.jpg")
+        img1 = img1.resize((400, 200), Image.ANTIALIAS)
+        self.photo_img1 = ImageTk.PhotoImage(img1)
+        bg_img = Label(self.screen, image=self.photo_img1)
+        bg_img.place(x=0, y=0)
+        Label(text="Train Data", font=("Print Bold", 24), fg="SteelBlue4",bg="powder blue").place(relx=0.35, rely=0.05)
+
+        # Tain_Button
+        train_button = ttk.Button(screen, command=self.train_dataset, text="Start Training Dataset", cursor="hand2")
+        train_button.place(relx=0.25, rely=0.45, width=200, height=50)
+
     # Function for Train Algorithm
     def train_dataset(self):
         data_dir = ("data")
@@ -34,5 +54,5 @@ class Train:
 
 if __name__ == "__main__":
     screen = Tk()
-    obj = Train(screen)
+    obj = Train_Dataset(screen)
     screen.mainloop()
