0e30dde4c8eb82d537cfec69d016e731300703e4
kiranbanstola <banstolakiran11@gmail.com>
2022-02-11 19:36:28 +0545

    Login With login Verify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 
 
@@ -7,48 +7,153 @@ class Attendance_System:
 
     def __init__(self, screen):
         self.screen = screen
-        self.screen.geometry("800x600+200+100")
-        self.screen.title("Attendance System")
+        self.screen.geometry("350x400+550+200")
+        self.screen.title("Login")
         self.screen.resizable(False, False)
 
         # bg_gradient
         img1 = Image.open(r"D:\FaceDetectionAndRecognition-v2\bg_gradient.jpg")
-        img1 = img1.resize((800, 600), Image.ANTIALIAS)
+        img1 = img1.resize((350, 400), Image.ANTIALIAS)
         self.photo_img1 = ImageTk.PhotoImage(img1)
         bg_img = Label(self.screen, image=self.photo_img1)
         bg_img.place(x=0, y=0)
 
-        Label(screen, text="Automatic Attendance System", font=("Montserrat Bold", 18), bg="pale turquoise",
-              fg="SteelBlue4").place(x=155, y=70)
+        # global username_verify
+        # global password_verify
 
-        global username_verify
-        global password_verify
+        # username_verify = StringVar()
+        # password_verify = StringVar()
 
-        username_verify = StringVar()
-        password_verify = StringVar()
+        # global username_entry1
+        # global password_entry1
 
-        global username_entry1
-        global password_entry1
+        # Main Login Frame
+        main_frame = Frame(self.screen, bg="white")
+        main_frame.place(x=50, y=50, width=250, height=300)
 
-        # Username Label
-        Label(text="Username", font=("Print Bold", 16)).place(x=213, y=138)
-        # Username Entry
-        username_entry1 = Entry(textvariable=username_verify)
-        username_entry1.place(x=213, y=165)
+        # Login Frame
+        login_frame = LabelFrame(main_frame, bd=2, text="Login", font=("Print Bold", 18), labelanchor="n", bg="White")
+        login_frame.place(x=10, y=10, width=230, height=280)
 
-        # Password Label
-        Label(text="Password", font=("Print Bold", 16)).place(x=213, y=214)
-        # Password Entry
-        password_entry1 = Entry(show='*', textvariable=password_verify)
-        password_entry1.place(x=213, y=236)
+        # Username Frame
+        uframe = Frame(login_frame, bg="white")
+        uframe.place(x=50, y=40, height=50, width=130)
+
+        # Username Label & Entry
+        User_label = Label(uframe, text="Username", font=("Print Bold", 14), bg="white")
+        User_label.pack()
+        self.username_entry = ttk.Entry(uframe)
+        self.username_entry.pack()
+
+        # Username Icon Image
+        uiconimg = Image.open(r"D:\FaceDetectionAndRecognition-v2\icons\Username_Frame.png")
+        self.User_icon = ImageTk.PhotoImage(uiconimg)
+        usericon = Label(uframe, image=self.User_icon, bg="white")
+        usericon.place(x=5, y=-2)
+
+        # Password Frame
+        pframe = Frame(login_frame, bg="white")
+        pframe.place(x=50, y=100, height=50, width=130)
+        # Password  Label & Entry
+        pass_label = Label(pframe, text="Password", font=("Print Bold", 14), bg="white")
+        pass_label.pack()
+        self.password_entry = ttk.Entry(pframe, show='*')
+        self.password_entry.pack()
+
+        # Password Icon Image
+        piconimg = Image.open(r"D:\FaceDetectionAndRecognition-v2\icons\Password_Frame.png")
+        self.pass_icon = ImageTk.PhotoImage(piconimg)
+        passwordicon = Label(pframe, image=self.pass_icon, bg="white")
+        passwordicon.place(x=5, y=0)
 
         # Login Button
-        Button(text="Login", padx=10, pady=5,
-               fg="white", bg="black", cursor="hand2").place(x=338,y=313)
+        lbutton = ttk.Button(login_frame, text="Login", cursor="hand2", command=self.login)
+        lbutton.place(x=125, y=190)
 
         # Register Button
-        Button(text="Register", padx=10, pady=5,
-               fg="white", bg="black", cursor="hand2").place(x=238,y=313)
+        rbutton = ttk.Button(login_frame, text="Register", cursor="hand2")
+        rbutton.place(x=25, y=190)
+
+    # Show Password
+    def show_password(self):
+        
+
+    # Login Function
+    def login(self):
+
+
+        # Function to show Error and success
+        def hide_username_error():
+            username_error_icon.place_forget()
+
+        def show_username_error():
+            global username_error_icon
+            username_error_iconimg = Image.open(r"D:\FaceDetectionAndRecognition-v2\icons\Error.png")
+            self.username_error_icon = ImageTk.PhotoImage(username_error_iconimg)
+            username_error_icon = Label(self.screen, image=self.username_error_icon, bg="white")
+            username_error_icon.place(x=240, y=151)
+
+
+
+        def show_password_error():
+            global password_erroricon
+            password_error_iconimg = Image.open(r"D:\FaceDetectionAndRecognition-v2\icons\Error.png")
+            self.password_error_icon = ImageTk.PhotoImage(password_error_iconimg)
+            password_erroricon = Label(self.screen, image=self.password_error_icon, bg="white")
+            password_erroricon.place(x=240, y=210)
+
+        def hide_password_error():
+            password_erroricon.place_forget()
+
+        def show_Label_error():
+            global errorlabel
+            errorlabel = Label(self.screen, text="*All Field Required", bg="white", fg="red")
+            errorlabel.place(x=120, y=240)
+
+        def hide_Label_error():
+            errorlabel.place_forget()
+
+        def show_Label_Success():
+            global successlabel
+            successlabel = Label(self.screen, text="Login Successfully", bg="white", fg="green")
+            successlabel.place(x=120, y=240)
+
+        def hide_Label_Success():
+            successlabel.place_forget()
+
+        def show_Label_UserNF():
+            global unf_errorlabel
+            unf_errorlabel = Label(self.screen, text="User Not Found", bg="white", fg="red")
+            unf_errorlabel.place(x=120, y=240)
+
+        def hide_Label_UserNF():
+            unf_errorlabel.place_forget()
+
+        def show_Label_invalidpass():
+            global invalid_errorlabel
+            invalid_errorlabel = Label(self.screen, text="Invalid password", bg="white", fg="red")
+            invalid_errorlabel.place(x=120, y=240)
+
+        def hide_Label_invalidpass():
+            invalid_errorlabel.place_forget()
+
+        if self.username_entry.get() == "":
+            if self.password_entry.get() == "":
+                show_username_error()
+                show_password_error()
+            else:
+
+                show_password_error()
+            show_Label_error()
+        else:
+            if self.username_entry.get() == "kapa":
+                if self.password_entry.get() == "123":
+                    show_Label_Success()
+                else:
+                    show_password_error()
+                    show_Label_invalidpass()
+            else:
+                show_Label_UserNF()
 
 
 
